cda103dc0da73146845741e1a882768c50f726cb
Artur Schaefer <artur.schaefer2@gmail.com>
2022-07-09 20:43:30 +0200

    update bfs algorithm with generic types

--- grokking-algorithms/06 - Breadth-first Search/01_BFS_Person.kt
+++ grokking-algorithms/06 - Breadth-first Search/01_BFS_Person.kt
@@ -1,9 +1,10 @@
 import java.util.Deque
 import java.util.LinkedList
+import kotlin.collections.last
 
 fun main() {
 
-    val graph = hashMapOf<Person, List<Person>>(
+    val valuesList = listOf(
         Person("you") to listOf(Person("alice"), Person("bob"), Person("claire")),
         Person("bob") to listOf(Person("anuj"), Person("peggy")),
         Person("alice") to listOf(Person("peggy")),
@@ -14,34 +15,47 @@ fun main() {
         Person("jonny") to emptyList(),
     )
 
-    searchMangoSeller(Person("you"), graph)
-}
+    val graph = Graph<Person>(valuesList)
+    val result = graph.breadthFirstSearch(Person("you"), Person::isMangoSeller)
 
-// O(V+E) (V for number of vertices, E for number of edges)
-fun searchMangoSeller(person: Person, graph: HashMap<Person, List<Person>>){
-    val searchQueue: Deque<Person> = LinkedList()
-    graph[person]?.let { 
-        searchQueue += it
-    }
-    val searched = HashSet<Person>()
-
-    while (searchQueue.isNotEmpty()){
-        val current = searchQueue.pop()
-
-        if (!searched.contains(current)){
-            if (current.isMangoSeller){
-                println("${current.name} is a Mango Seller")
-                return
-            } else {
-                graph[current]?.let { 
-                    searchQueue += it
-                }
-                searched.add(current)
-            }
-        }
-    }
+    result?.let { 
+        println("${it.last().name} is a Mango Seller")
+        println("Searched graph -> ${result.toString()}") 
+    } ?: println("There is no Mango seller")
 }
 
 data class Person(val name: String, val isMangoSeller: Boolean = false) {
     override fun equals(other: Any?): Boolean = if (other is Person) other.name == name else false
+}
+
+class Graph<V>(val values: List<Pair<V, List<V>>>){
+
+    // O(V+E) (V for number of vertices, E for number of edges)
+    fun breadthFirstSearch(key: V, searchFunction: (V) -> Boolean): List<V>?{
+        val graph = hashMapOf<V, List<V>>()
+        graph.putAll(values)
+
+        var searchQueue: Deque<V> = LinkedList()
+        graph[key]?.let { 
+            searchQueue += it
+        }
+        val searched = LinkedHashSet<V>()
+    
+        while (searchQueue.isNotEmpty()){
+            val current = searchQueue.pop()
+    
+            if (!searched.contains(current)){
+                if (searchFunction(current)){
+                    searched.add(current)
+                    return searched.toList()
+                } else {
+                    graph[current]?.let { 
+                        searchQueue += it
+                    }
+                    searched.add(current)
+                }
+            }
+        }
+        return null
+    }
 }
