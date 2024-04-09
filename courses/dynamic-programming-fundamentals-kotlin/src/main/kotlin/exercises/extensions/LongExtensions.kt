938478c22f825084abf193013c75048ac688c518
DESKTOP-JFU8E24\Artur Schaefer <artur.schaefer2@gmail.com>
2024-04-09 15:37:51 +0200

    remove unused projects

--- courses/dynamic-programming-fundamentals-kotlin/src/main/kotlin/exercises/extensions/LongExtensions.kt
+++
@@ -1,5 +0,0 @@
-package exercises.extensions
-
-private const val NANOSECONDS_IN_SECONDS = 1_000_000_000f
-
-fun Long.nanoTimeToSecondsString() = String.format("%.8f", this / NANOSECONDS_IN_SECONDS)
--- courses/dynamic-programming-fundamentals-kotlin/src/main/kotlin/exercises/fibonacci/01-Fibonacci.kt
+++
@@ -1,7 +0,0 @@
-package exercises.fibonacci
-
-fun main(){
-    printFibonacciNaiveResult(listOf(20, 30, 40))
-    printFibonacciMemoizationResult(listOf(50, 100, 1000))
-    printFibonacciBottomUp(listOf(50, 1000))
-}
